cbf71032f2a148ddef4c594560eac07e9961857f
Jxnkk <jxzkuang@gmail.com>
2025-05-30 16:34:33 +0000

    :(

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -1,12 +1,9 @@
-//Connecting to css file for styling 
-import "./Clock.css"; 
-
 export default function Clock() {
     return (
         <div>
             <iframe
                 //Connects to the Pomodoro timer from Flocus
-                src = "https://flocus.com/minimalist-pomodoro-timer"
+                src = "https://studywithme.io/aesthetic-pomodoro-timer/"
                 allowFullScreen
             />
         </div>
